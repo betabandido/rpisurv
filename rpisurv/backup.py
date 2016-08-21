d5eff48f3fac49ad19934ec7632388c34051c67e
Victor Jimenez <betabandido@gmail.com>
2016-08-21 10:15:42 +0000

    Code cleanup and documentation.

--- rpisurv/backup.py
+++ rpisurv/backup.py
@@ -17,32 +17,79 @@ def _build_service(credentials):
   http = credentials.authorize(Http())
   return build('drive', 'v3', http=http)
 
-def upload_jpeg_file(filename):
-  service = _build_service(get_credentials())
-  folder_metadata = {
-    'name': datetime.now().strftime('D%d-%m-%yT%I%p'),
-    'mimeType': 'application/vnd.google-apps.folder'
-  }
+def _find_folder(service, name):
+  """Find a folder in Drive.
 
-  # try to find folder
-  folder = service.files().list(
+  Args:
+    service: The Drive service object.
+    name: The folder name.
+
+  Returns:
+    The folder object.
+  """
+  result = service.files().list(
       q="name='{0}' and mimeType='{1}' and trashed=false".format(
-        folder_metadata['name'],
-        folder_metadata['mimeType']),
+        name,
+        'application/vnd.google-apps.folder'),
       spaces='drive',
       fields='files(id)').execute()
-  if len(folder['files']) == 0:
-    # create folder if it doesn't exist
-    folder = service.files().create(
-        body=folder_metadata,
-        fields='id').execute()
-  elif len(folder['files']) == 1:
-    folder = folder['files'][0]
-  else:
-    raise Exception('Duplicated name')
+  return result['files']
 
-  print('Folder ID: {0}'.format(folder['id']))
+def _create_folder(service, name):
+  """Create a folder in Drive.
 
+  Args:
+    service: The Drive service object.
+    name: The folder name.
+
+  Returns:
+    The folder object.
+  """
+  folder_metadata = {
+    'name': name,
+    'mimeType': 'application/vnd.google-apps.folder'
+  }
+  folder = service.files().create(
+      body=folder_metadata,
+      fields='id').execute()
+  return folder
+
+def _create_unique_folder(service, name):
+  """Create a folder in Drive only if it does not exist yet.
+
+  If the folder already exists, this method returns the folder object.
+
+  Args:
+    service: The Drive service object.
+    name: The folder name.
+
+  Returns:
+    The folder object for the created or existing folder.
+  """
+  files = _find_folder(service, name)
+  if len(files) == 1:
+    return files[0]
+
+  if len(files) != 0:
+    raise Exception('Duplicated folder name')
+
+  return _create_folder(service, name)
+
+def upload_jpeg_file(filename):
+  """Uploads a JPEG file to Drive.
+
+  The file will be placed in a folder for better organization. The folder name
+  is the current date and hour. That means all the files uploaded within the
+  same hour will be uploaded to the same folder.
+
+  Args:
+    filename: Name of the file to upload.
+  """
+  service = _build_service(get_credentials())
+  folder_name = datetime.now().strftime('D%d-%m-%yT%I%p')
+  folder = _create_unique_folder(service, folder_name)
+
+  print('Uploading image to folder ID: {0}'.format(folder['id']))
   media_body = MediaFileUpload(filename, mimetype='image/jpeg')
   service.files().create(
       body={'name': os.path.basename(filename), 'parents': [folder['id']]},
