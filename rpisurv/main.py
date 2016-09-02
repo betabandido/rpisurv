c73a1dc850f1b82cf1858e6d164eeaba8ea51c03
Victor Jimenez <betabandido@gmail.com>
2016-09-02 08:56:45 +0000

    Use python-daemon to run as a background task.

--- rpisurv/main.py
+++ rpisurv/main.py
@@ -1,11 +1,12 @@
 from backup import upload_jpeg_file
+from daemon import runner
 from motion import MotionDetector
 import os
-from PIL import Image
 from picamera import PiCamera
 from picamera.array import PiRGBArray
+from PIL import Image
 from sendmail import MotionNotifier
-import time
+from time import sleep
 import yaml
 
 basepath = os.path.dirname(os.path.realpath(__file__))
@@ -14,40 +15,74 @@ with open(os.path.join(basepath, 'settings.yaml')) as f:
   print str(settings)
 
 camera_settings = settings['camera']
-camera = PiCamera()
-camera.hflip = True
-camera.vflip = True
-camera.resolution = tuple(camera_settings['resolution'])
-camera.framerate = camera_settings['fps']
-raw_capture = PiRGBArray(camera, size=tuple(camera_settings['resolution']))
 
-motion_detector = MotionDetector(settings['motion'])
-motion_notifier = MotionNotifier(settings['notification'])
+def create_camera():
+  camera = PiCamera()
+  camera.hflip = True
+  camera.vflip = True
+  camera.resolution = tuple(camera_settings['resolution'])
+  camera.framerate = camera_settings['fps']
+  return camera
 
-time.sleep(camera_settings['warmup_time'])
+terminated = False
 
-print('Surveilling')
+def terminate_daemon(signal_number, stack_frame):
+  global terminated
+  terminated = True
 
-image_count = 0
+class Application:
+  def __init__(self):
+    app_settings = settings['app']
+    self.stdin_path = '/dev/null'
+    self.stdout_path = app_settings['out_path']
+    self.stderr_path = app_settings['err_path']
+    self.pidfile_path = app_settings['pidfile_path']
+    self.pidfile_timeout = 5
 
-for frame in camera.capture_continuous(
-    raw_capture,
-    format='bgr',
-    use_video_port=True):
-  if motion_detector.detect_motion(frame.array):
-    print('*** MOTION DETECTED ***')
-    try:
-      motion_notifier.send_notification()
-      img = Image.fromarray(frame.array, 'RGB')
-      filename = os.path.join(
-          settings['backup']['output_path'],
-          'motion{0:04d}.jpg'.format(image_count))
-      img.save(filename)
-      upload_jpeg_file(filename)
-      os.remove(filename)
-      image_count += 1
-    except Exception, error:
-      print('An error occurred: {0}'.format(error))
+  def run(self):
+    print('Initializing')
+    motion_detector = MotionDetector(settings['motion'])
+    motion_notifier = MotionNotifier(settings['notification'])
 
-  raw_capture.truncate(0)
+    with create_camera() as camera:
+      raw_capture = PiRGBArray(camera, size=camera.resolution)
+      sleep(camera_settings['warmup_time'])
+
+      print('Surveilling')
+      image_count = 0
+      for frame in camera.capture_continuous(
+          raw_capture,
+          format='bgr',
+          use_video_port=True):
+        global terminated
+        if terminated:
+          break
+
+        if motion_detector.detect_motion(frame.array):
+          print('*** MOTION DETECTED ***')
+          try:
+            motion_notifier.send_notification()
+            img = Image.fromarray(frame.array, 'RGB')
+            filename = os.path.join(
+                settings['backup']['output_path'],
+                'motion{0:04d}.jpg'.format(image_count))
+            img.save(filename)
+            upload_jpeg_file(filename)
+            os.remove(filename)
+            image_count += 1
+          except Exception, error:
+            print('An error occurred: {0}'.format(error))
+
+        raw_capture.truncate(0)
+
+    print('Exiting')
+    # This seems necessary to kill threads created by libbcm_host
+    os.killpg(os.getpgid(0), signal.SIGKILL)
+
+if __name__ == '__main__':
+  import signal
+  app = Application()
+  daemon_runner = runner.DaemonRunner(app)
+  daemon_runner.daemon_context.signal_map[signal.SIGTERM] = terminate_daemon
+  daemon_runner.do_action()
 
